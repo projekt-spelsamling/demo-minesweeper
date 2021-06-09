e8876fbb77c4b79d154007d86563a52fac7ba6ac
Denkaan <dennis.petreski@hotmail.com>
2021-06-09 17:16:08 +0200

    Added new main to build a JAR File and updated the README on how to build it.

--- src/main/java/edu/agile/JavaFX.java
+++ src/main/java/edu/agile/JavaFX.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class JavaFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -23,7 +23,7 @@ public class App extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void runJavaFX(String[] args) {
         launch();
     }
 
